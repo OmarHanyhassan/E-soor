d8f0fc8ce6eb0c810b7e637f4e6206d4ddea6edb
Omar Yehia <omaryahiatawfiek@gmail.com>
2020-02-25 20:45:32 +0200

    Updating some lines

--- lib/services/api.dart
+++ lib/services/api.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart' as http;
 var users;
 
 getUsers () async {
-	var req = await http.get("");
+	var req = await http.get("http://my-json-server.typicode.com/OmarYehiaDev/E-Soor/db");
 	users = convert.jsonDecode(req.body);
 	users.forEach((user){
 		return user;
