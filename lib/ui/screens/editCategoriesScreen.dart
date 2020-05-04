728f00f09b8e1104b88898573bba6b8314aeedaa
Omar Yehia <omaryahiatawfiek@gmail.com>
2020-05-04 04:27:21 +0200

    UI Improvements

--- lib/ui/screens/editCategoriesScreen.dart
+++ lib/ui/screens/editCategoriesScreen.dart
@@ -11,13 +11,32 @@ class EditCategoriesScreen extends StatefulWidget {
 class _EditCategoriesScreenState extends State<EditCategoriesScreen> {
   TextEditingController titleController;
   TextEditingController listController;
-  static String stringLists = "";
-  List<String> lists;
+  List<String> tags = [];
+  final globalKey = GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
+    final snackBar = SnackBar(
+      content: Text(
+        "Saved successfully",
+        textAlign: TextAlign.center,
+        style: Theme.of(context).textTheme.subhead.copyWith(
+              fontWeight: FontWeight.bold,
+            ),
+      ),
+      backgroundColor: Colors.green,
+    );
     return Scaffold(
+      key: globalKey,
       appBar: AppBar(
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.save),
+            onPressed: () {
+              globalKey.currentState.showSnackBar(snackBar);
+            },
+          ),
+        ],
         leading: IconButton(
           icon: Icon(Icons.cancel),
           onPressed: () {
@@ -27,22 +46,92 @@ class _EditCategoriesScreenState extends State<EditCategoriesScreen> {
         title: Text("Edit " + widget.category.name),
       ),
       body: Container(
-        height: 400,
+        height: MediaQuery.of(context).size.height * 0.5,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-            TextFormField(
-              controller: titleController,
-              textCapitalization: TextCapitalization.words,
-              initialValue: widget.category.name,
-              onFieldSubmitted: (value) {
-                setState(() {
-                  titleController.text = value;
-                });
-              },
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 8.0,
+                    horizontal: 15.0,
+                  ),
+                  child: Text(
+                    "Edit Category name :",
+                    style: Theme.of(context).textTheme.title,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                  child: TextField(
+                    controller: titleController,
+                    textCapitalization: TextCapitalization.words,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25.0),
+                        borderSide: BorderSide(
+                          color: Colors.white,
+                        ),
+                      ),
+                      hasFloatingPlaceholder: true,
+                      hintText: widget.category.name,
+                    ),
+                    onSubmitted: (value) {
+                      setState(() {
+                        titleController.text = value;
+                      });
+                    },
+                  ),
+                ),
+              ],
             ),
-            TextFormField(
-              controller: listController,
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 8.0,
+                    horizontal: 15.0,
+                  ),
+                  child: Text(
+                    "Edit SubCategories :",
+                    style: Theme.of(context).textTheme.title,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                  child: TextField(
+                    textCapitalization: TextCapitalization.words,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25.0),
+                        borderSide: BorderSide(
+                          color: Colors.white,
+                        ),
+                      ),
+                      hasFloatingPlaceholder: true,
+                    ),
+                    controller: listController,
+                    onSubmitted: (value) {
+                      setState(() {
+                        tags.addAll(value.split(","));
+                      });
+                    },
+                  ),
+                ),
+              ],
+            ),
+            Wrap(
+              children: tags.map(
+                (tag) {
+                  return Chip(
+                    label: Text(tag),
+                    deleteIcon: Icon(Icons.cancel),
+                  );
+                },
+              ).toList(),
             ),
           ],
         ),
